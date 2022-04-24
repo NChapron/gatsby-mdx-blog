c65144f8ba6152844241a48bc65f69ea5d949991
Nicolas Chapron <nicolas.chapron2022@gmail.com>
2022-04-24 22:50:13 +0100

    Added individual posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
 exports.createPages = async function ({ actions, graphql }) {
-  const { data } = await graphql (`
+  const { data } = await graphql(`
     query {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
@@ -34,12 +34,13 @@ exports.createPages = async function ({ actions, graphql }) {
   })
 
   // Create single blog posts
-/*   data.allMdx.edges.forEach(edge => {
+  data.allMdx.edges.forEach(edge => {
     const slug = edge.node.frontmatter.slug
-    const id = edge.node.idactions.createPages({
+    const id = edge.node.id
+    actions.createPage({
       path: slug,
       component: require.resolve(`./src/templates/singlePost.js`),
       context: { id },
     })
-  }) */
+  })
 }
